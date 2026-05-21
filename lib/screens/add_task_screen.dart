3ec645fef1398d0ef1b6cbf9fa73ebe61f3ad4f2
SREERUP MOJUMDER <vhopel.sm@gmail.com>
2026-05-22 04:24:22 +0600

    Done add task elevated button

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -25,104 +25,126 @@ class AddTaskScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
+          mainAxisAlignment: .spaceBetween,
           children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    offset: Offset(0, 4),
-                    blurRadius: 5,
+            Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        offset: Offset(0, 4),
+                        blurRadius: 5,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  //Icon
-                  Container(
-                    padding: EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                      color: Colors.purpleAccent.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Icon(
-                      Icons.business_center,
-                      color: Colors.purpleAccent,
-                      size: 18,
-                    ),
-                  ),
-                  SizedBox(width: 15),
-                  Column(
+                  child: Row(
                     children: [
+                      //Icon
+                      Container(
+                        padding: EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                          color: Colors.purpleAccent.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Icon(
+                          Icons.business_center,
+                          color: Colors.purpleAccent,
+                          size: 18,
+                        ),
+                      ),
+                      SizedBox(width: 15),
                       Column(
-                        crossAxisAlignment: .start,
                         children: [
-                          Text(
-                            "Task Group",
-                            style: TextStyle(
-                              color: Colors.black38,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                          Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                "Task Group",
+                                style: TextStyle(
+                                  color: Colors.black38,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
 
-                          Text(
-                            'Work',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
+                              Text(
+                                'Work',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(12)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.1),
-                    offset: Offset(0, 4),
-                    blurRadius: 5,
+                ),
+                SizedBox(height: 30),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        offset: Offset(0, 4),
+                        blurRadius: 5,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: .start,
-                children: [
-                  Text(
-                    "Description",
-                    style: TextStyle(
-                      color: Colors.black38,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 14,),
-                  Text(
-                    "This application is designed for super shops. By using this application they can enlist all their products in one place and can deliver. Customers will get a one-stop solution for their daily shopping",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    children: [
+                      Text(
+                        "Description",
+                        style: TextStyle(
+                          color: Colors.black38,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 14,),
+                      Text(
+                        "This application is designed for super shops. By using this application they can enlist all their products in one place and can deliver. Customers will get a one-stop solution for their daily shopping",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
 
-                    ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+
+              ],
             ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: ElevatedButton(onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white
+                  ),
+                  child: Text('Add', style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),)),
+            )
           ],
         ),
       ),
